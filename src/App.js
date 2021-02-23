1011710a357ca8a2b250f0a78562d2fc8326e0ce
sambitos23 <sambit28saha@gmail.com>
2021-02-23 15:34:11 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box_Shade from "../src/tailwind_css/Tailwind_box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>hello from Sambit</h1>
+        <Box_Shade/>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/tailwind_css/Tailwind_box.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default class Box_Shade extends React.Component{
+  render(){
+    return(
+      <div>
+        <div className="w-5 h-6 bg-blue-100">TEXT</div>
+        <div className="w-5 h-6 bg-blue-200">TEXT</div>
+        <div className="w-5 h-6 bg-blue-300">TEXT</div>
+        <div className="w-5 h-6 bg-blue-400">TEXT</div>
+        <div className="w-5 h-6 bg-blue-500">TEXT</div>
+        <div className="w-5 h-6 bg-blue-600">TEXT</div>
+        <div className="w-5 h-6 bg-blue-700">TEXT</div>
+        <div className="w-5 h-6 bg-blue-800">TEXT</div>
+        <div className="w-5 h-6 bg-blue-900">TEXT</div>
+      </div>
+    )
+  }
+}
