992f4de8fcf8dbecb8a9e5098280da435550918f
sambitos23 <sambit28saha@gmail.com>
2021-02-27 16:04:03 +0530

    input style

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Box_Shade from "../src/tailwind_css/Tailwind_box";
 import Text from "../src/tailwind_css/Text_style";
 import Button_style from "../src/tailwind_css/Buttons_style";
 import Block from "../src/tailwind_css/Block_gallery";
+import Input from "../src/tailwind_css/input_style";
 
 
 export default class App extends React.Component{
   render(){
     return(
       <div>
-        hello from Sambit
+        {/* hello from Sambit
         <Box_Shade/><br /><br />
         <Text/><br /><br />
         <Button_style /><br/><br />
-        <Block/>
+        <Block/> */}
+        <Input />
       </div>
     )
   }
---
+++ src/tailwind_css/input_style.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default class Input extends React.Component{
+  render(){
+    return(
+      <div className="h-screen flex flex-col justify-center items-center">
+              <label for="full_name" className="text-gray-700 text-sm font-bold select-none">
+                  Full Name
+              </label>
+              <input id="full_name" placeholder="Enter your full name" class="mt-2 shadow border rounded-lg w-4/6 px-3 py-2 text-gray-700 focus:bg-blue-100 placeholder-indigo-300" />
+              <button class="mt-2 px-3 py-2 bg-blue-900 text-blue-100 rounded-lg">Save</button>
+      </div>
+    )
+  }
+}
